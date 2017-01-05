e3d7f7fb174bbe668437ff46bd8b851f27db2460
judahnator <judah@thosetechguys.org>
2017-01-05 17:25:03 -0600

    input stuffs

--- pages/UserAdd.php
+++ pages/UserAdd.php
@@ -1,7 +1,30 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: judah
- * Date: 1/5/17
- * Time: 5:15 PM
- */
+<div class="wrap">
+    <div id="icon-users" class="icon32"></div>
+    <h2>Add New User</h2>
+
+    <form action="" method="post">
+        <table class="form-table">
+            <tbody>
+            <tr>
+                <td>Name:</td>
+                <td>
+                    <input type="text" name="name">
+                </td>
+            </tr>
+            <tr>
+                <td>Email Address<small>(optional)</small>:</td>
+                <td>
+                    <input type="email" name="email">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <p class="submit">
+                        <input type="submit" value="Add User" class="button-primary" name="Submit">
+                    </p>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+</div>
