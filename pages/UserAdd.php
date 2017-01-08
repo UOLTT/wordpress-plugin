40b3612d1bb25fba36d7e1ddfd3773409978f81d
judahnator <judah@thosetechguys.org>
2017-01-08 10:44:33 -0600

    handling the ship adding/whatever in the backend because its easier that way

--- pages/UserAdd.php
+++ pages/UserAdd.php
@@ -1,12 +1,43 @@
 <?php
+
+use GuzzleHttp\Exception\ClientException;
+
 $GuzzleClient = new GuzzleHttp\Client(['base_uri'=>'https://api.uoltt.org/api/v4/']);
+
 $Response = $GuzzleClient->get('ships');
 $Ships = json_decode($Response->getBody());
+
+if (array_key_exists('Submit',$_POST)) {
+    try{
+        $Response = $GuzzleClient->post('users',['form_params'=>[
+            'name' => $_POST['name'],
+            'username' => $_POST['username'],
+            'game_handle' => $_POST['game_handle'],
+            'email' => $_POST['email'],
+            'password' => '',
+            'ships' => $_POST['ships'],
+            'organization_id' => 1
+        ]]);
+        $ResponseBody = json_decode($Response->getBody());
+        ?>
+        <div class="notice notice-success is-dismissible" id="success_message">
+            <p><?= $ResponseBody->username; ?> has been successfully added with <?= count($ResponseBody->ships); ?> ships!</p>
+        </div>
+        <?php
+    }catch (ClientException $e) {
+        $Response = json_decode($e->getResponse()->getBody());
+        ?>
+        <div class="notice notice-error is-dismissible" id="error_message">
+            <p><?= $Response->error; ?></p>
+        </div>
+        <?php
+    }
+}
+
 ?>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-<div class="notice-success is-dismissible" id="success_message" style="display: none"></div>
-<div class="notice-error is-dismissible" id="error_message" style="display: none"></div>
+<div class="notice notice-error is-dismissible" id="error_message" style="display: none"></div>
 
 <div class="wrap">
     <div id="icon-users" class="icon32"></div>
@@ -18,25 +49,25 @@ $Ships = json_decode($Response->getBody());
             <tr>
                 <td>Name:<small>(optional)</small>:</td>
                 <td>
-                    <input type="text" id="name">
+                    <input type="text" name="name">
                 </td>
             </tr>
             <tr>
                 <td>Email Address<small>(optional)</small>:</td>
                 <td>
-                    <input type="email" id="email">
+                    <input type="email" name="email">
                 </td>
             </tr>
             <tr>
                 <td>Username:</td>
                 <td>
-                    <input type="text" id="username">
+                    <input type="text" name="username">
                 </td>
             </tr>
             <tr>
                 <td>Game Handle:</td>
                 <td>
-                    <input type="text" id="game_handle">
+                    <input type="text" name="game_handle">
                 </td>
             </tr>
             <tr>
@@ -44,13 +75,13 @@ $Ships = json_decode($Response->getBody());
                 <td>
                     <input id="shipsList"><br>
                     <button type="button" class="button-primary">Add Ship</button><br>
-                    <select multiple disabled id="ships" style="width: 218px"></select>
+                    <select multiple readonly name="ships[]" id="ships" style="width: 218px"></select>
                 </td>
             </tr>
             <tr>
                 <td colspan="2">
                     <p class="submit">
-                        <button type="button" onclick="AddUser()" class="button-primary">Add User</button>
+                        <input type="submit" class="button-primary" name="Submit" value="Add User">
                     </p>
                 </td>
             </tr>
@@ -92,25 +123,4 @@ $Ships = json_decode($Response->getBody());
             jQuery('#ships').html() + '<option selected value="' + ShipIdMap[ShipName] + '">' + ShipName + '</option>'
         );
     });
-
-    function AddUser() {
-        jQuery.post('https://api.uoltt.org/api/v4/users',{
-            name: jQuery('#name').val(),
-            username: jQuery('#username').val(),
-            game_handle: jQuery('#game_handle').val(),
-            email: jQuery('#email').val(),
-            password: '',
-            ships: jQuery('#ships').val()
-        }).always(function(data) {
-            if (data.error == undefined) {
-                jQuery('#success_message').html('<p>User has been successfully added</p>');
-                jQuery('#error_message').hide();
-                jQuery('#success_message').show();
-            }else {
-                jQuery('#error_message').html('<p>An error has occured. Either you didnt enter in all the data or you need to bug judahnator.</p>');
-                jQuery('#success_message').hide();
-                jQuery('#error_message').show();
-            }
-        });
-    }
 </script>
